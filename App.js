d03d5c04b826c9590c6e8129779d286e07967f31
gmagnere <103102555+gmagnere@users.noreply.github.com>
2022-05-24 16:54:33 +0200

    feat: Ajout Forecast

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet, FlatList, ActivityIndicator, Image } from 'react-native';
+import { Platform, Text, View, StyleSheet, FlatList, ActivityIndicator, Image, ImageBackground } from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
+  const image = {uri: "https://i.stack.imgur.com/jGlzr.png"};
   const [location, setLocation] = useState(null);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
@@ -10,6 +11,7 @@ export default function App() {
 
   //const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [forecast, setForecast] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -46,8 +48,48 @@ export default function App() {
         });
   }
 
+  const getForecast = async () => {
+    //console.log(latitude)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.coords.latitude}&lon=${location.coords.longitude}&exclude={part}&appid=8d2123a11118353aa3c434fa7e8678c5&units=metric&lang=fr`)
+        .then(response => response.json())
+        .then(forecast => {
+          setForecast(forecast);
+          console.log(forecast)
+        });
+  }
+
+  const Item = ({ title }) => (
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+  )
+
+  const renderItem = ({ item }) => (
+      <View >
+            <Text>
+             <Item title={'Température en journée : '+item.temp.morn} />
+            </Text>
+              <Text>
+                  <Item title={'Température l\'après-midi : '+item.temp.eve} />
+              </Text>
+              <Text>
+                  <Item title={'Température en matinée : '+item.temp.day} />
+              </Text>
+              <Text>
+                  <Item title={'Température en soirée : '+item.temp.night} />
+              </Text>
+              <Text>
+                  <Item title={'Température Max : '+item.temp.max} />
+              </Text>
+              <Text>
+                  <Item title={'Température en minimal : '+item.temp.min} />
+              </Text>
+      </View>
+  );
+
   useEffect(() => {
     location && getCity();
+    location && getForecast();
   }, [location]);
 
   let text = <ActivityIndicator size="small" color="#0000ff" />;
@@ -59,34 +101,61 @@ export default function App() {
 
   return (
       <View style={styles.container}>
-        {data.weather && (
-            <>
-              {text}
-              <Text>Vous êtes à : {data.name}</Text>
-              <Text>Il fait : {data.main?.temp} c°</Text>
-              <Text>Le temps : {data.weather[0].description} </Text>
-              <Image
-                  style={styles.tinyLogo}
-                  source = {{ uri: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
-                //uri: 'http://openweathermap.org/img/w/01n.png'
-              }}/>
-            </>
-          )
-        }
-      </View>
-
-  );
+          <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+                <View style={styles.text}>
+                    {data.weather && (
+                        <>
+                            {text}
+                            <FlatList
+                                data={forecast.daily}
+                                renderItem={renderItem}
+                                keyExtractor={item => item.dt}
+                            />
+                            <Text>Vous êtes à : {data.name}</Text>
+                            <Text>Il fait : {data.main?.temp} c°</Text>
+                            <Text>Le temps : {data.weather[0].description} </Text>
+                            <Image
+                                style={styles.tinyLogo}
+                                source = {{ uri: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+                                    //uri: 'http://openweathermap.org/img/w/01n.png'
+                                }}/>
+                        </>
+                    )
+                    }
+                </View>
+            </ImageBackground>
+          </View>
+          );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
   tinyLogo: {
     width: 50,
     height: 50,
   },
+
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+  },
+
+    text: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+
+    item: {
+        backgroundColor: '#156988',
+        padding: 20,
+        marginTop: 30,
+        marginVertical: 1,
+        marginHorizontal: 16,
+        flexDirection: "row",
+    },
 });
